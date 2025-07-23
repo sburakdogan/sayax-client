179fe8773c0439444f93ee2aa2024cc2f8836dca
Burak Dogan <46339962+sburakdogan@users.noreply.github.com>
2025-07-23 21:19:17 +0300

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { AmountList } from './components/amount-list/amount-list';
 
 export const routes: Routes = [
-    { path: 'amounts', component: AmountList }
+    { path: 'amounts', component: AmountList },
+    { path: '', component: AmountList }
 ];
